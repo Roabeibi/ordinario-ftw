d5fb1402c922bd3ba9eb522b7c0fcb4d94fef2a9
Roabeibi <alessandrozra@gmail.com>
2026-06-07 00:45:31 -0600

    Actualizacion del Login xml - 8

--- EstancionamientoRoa/wwwroot/js/Login.js
+++ EstancionamientoRoa/wwwroot/js/Login.js
@@ -1,32 +1,31 @@
-async function validarLogin() {
-
-    let usuario = document.getElementById("usuario").value;
-    let password = document.getElementById("password").value;
-    let respuesta = await fetch("/xml/usuarios.xml");
-    let texto = await respuesta.text();
-    let parser = new DOMParser();
-    let xml = parser.parseFromString(texto, "text/xml");
-    let usuarios = xml.getElementsByTagName("usuario");
-    let valido = false;
-    for (let i = 0; i < usuarios.length; i++) {
-        let nombre = usuarios[i]
-            .getElementsByTagName("nombre")[0]
-            .textContent;
-
-        let pass = usuarios[i]
-            .getElementsByTagName("password")[0]
-            .textContent;
-
-        if (usuario == nombre && password == pass) {
-            valido = true;
-        }
-    }
- let mensaje = document.getElementById("mensaje");
-
- if (valido) {
-        mensaje.innerHTML = "Usuario correcto";
-    }
- else {
-        mensaje.innerHTML = "Usuario incorrecto";
-    }
+document.getElementById("formLogin")
+.addEventListener("submit",async function(event){
+event.preventDefault();
+let usuario=document.getElementById("usuario").value;
+let password=document.getElementById("password").value;
+let resultado=document.getElementById("resultadoLogin");
+let respuesta=await fetch("/xml/usuarios.xml");
+let texto=await respuesta.text();
+let parser=new DOMParser();
+let xml=parser.parseFromString(texto,"text/xml");
+let usuarios=xml.getElementsByTagName("usuario");
+let valido=false;
+for(let i=0;i<usuarios.length;i++){
+let nombre=usuarios[i]
+.getElementsByTagName("nombre")[0]
+.textContent.trim();
+let pass=usuarios[i]
+.getElementsByTagName("password")[0]
+.textContent.trim();
+if(usuario.trim()==nombre &&
+password.trim()==pass){
+valido=true;
 }
+}
+if(valido){
+resultado.innerHTML="Inicio de sesion correcto";
+}
+else{
+resultado.innerHTML="Usuario o contraseña incorrectos";
+}
+});
